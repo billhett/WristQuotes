a84716f9af58258604b5d099dbb4ce56f3700117
William Hettinger <bill.hettinger@gmail.com>
2018-03-22 17:56:52 +0100

    set up table with rows as a class

--- WristQuotes WatchKit Extension/RowInterfaceController.swift
+++ WristQuotes WatchKit Extension/RowInterfaceController.swift
@@ -1,5 +1,5 @@
 //
-//  InterfaceController.swift
+//  RowInterfaceController.swift
 //  WristQuotes WatchKit Extension
 //
 //  Created by William Hettinger on 3/22/18.
@@ -10,30 +10,22 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class RowInterfaceController: WKInterfaceController {
 
-    @IBOutlet var table: WKInterfaceTable!
-    var quotes = ["quote 1", "quote 2"]
+    @IBOutlet var quoteLabel: WKInterfaceLabel!
+    
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
-        self.table.setNumberOfRows(self.quotes.count, withRowType: "RowInterfaceController")
-        var count = 0
-        for quote in self.quotes {
-            var row = table.rowController(at: count) as! RowInterfaceController
-            row.quoteLabel.setText(quote)
-            count += 1
-        }
-        
     }
-    
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-    
+
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
